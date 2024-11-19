54a9f4cc0a042379a7e1e22fc66db6fa4962ab32
Marsit Group <gerlandtahoni972@gmail.com>
2024-11-19 17:56:21 +0700

    tes

---
+++ variabel.php
@@ -0,0 +1,8 @@
+<?php
+$namaD = "Gerland";
+$namaB = "Tahoni";
+$nim = "312310712";
+$prodi = "Teknik";
+$jurusan = "Teknik Informatika";
+$kelas = "TI23 CA1";
+$kampus = "Universitas Pelita Bangsa";
